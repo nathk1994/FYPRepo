86a781b8f442d2706e3bc852be890cb18ee57d4f
nathk1994 <72452040+nathk1994@users.noreply.github.com>
2022-04-01 15:13:07 +0100

    Resolve Production application crash on launch 2
    Implemented the remaining missing relative paths.

--- server/accounts/account.service.js
+++ server/accounts/account.service.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const crypto = require("crypto");
 const { Op } = require('sequelize');
-const sendEmail = require('_helpers/send-email');
-const db = require('_helpers/db');
-const Role = require('_helpers/role');
+const sendEmail = require('./_helpers/send-email');
+const db = require('./_helpers/db');
+const Role = require('./_helpers/role');
 
 module.exports = {
     authenticate,
