df417dbfb300515f626102fe330fdb8753c0ddb2
nathk1994 <72452040+nathk1994@users.noreply.github.com>
2022-04-06 01:28:45 +0100

    Improve Login page
    Added an Image and Title/welcome message, to login page.
    Added .scss file for login component.

--- client/src/app/account/login.component.ts
+++ client/src/app/account/login.component.ts
@@ -5,7 +5,7 @@ import { first } from 'rxjs/operators';
 
 import { AccountService, AlertService } from '@app/_services';
 
-@Component({ templateUrl: 'login.component.html' })
+@Component({ templateUrl: 'login.component.html' , styleUrls: ['./login.component.scss'] })
 export class LoginComponent implements OnInit {
     form: FormGroup;
     loading = false;
